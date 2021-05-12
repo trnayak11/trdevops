84e31c71c58ebd37dbdaec0ddf5dccd405f80a38
trupti <trnayak11@gmail.com>
2021-05-12 09:41:29 +0530

    12 may

--- project/src/main/java/mvn/spel/Teacher.java
+++ project/src/main/java/mvn/spel/Teacher.java
@@ -9,6 +9,40 @@ public class Teacher {
 	private int x;
 	@Value("#{5+4+7}")
 	private int y;
+	@Value("#{T(java.lang.Math).sqrt(25)}")
+	private double z;
+	@Value("#{T(java.lang.Math).E}")
+	private double e;
+	@Value("#{new java.lang.String('NSZ highschool sunderpur')}")
+	private String school;
+	@Value("#{8>3}")
+	private boolean isActive;
+	
+	
+	public String getSchool() {
+		return school;
+	}
+	public void setSchool(String school) {
+		this.school = school;
+	}
+	public boolean isActive() {
+		return isActive;
+	}
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+	public double getE() {
+		return e;
+	}
+	public void setE(double e) {
+		this.e = e;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
 	public int getX() {
 		return x;
 	}
@@ -23,7 +57,10 @@ public class Teacher {
 	}
 	@Override
 	public String toString() {
-		return "Teacher [x=" + x + ", y=" + y + "]";
+		return "Teacher [x=" + x + ", y=" + y + ", z=" + z + ", e=" + e + ", school=" + school + ", isActive="
+				+ isActive + "]";
 	}
+	
+	
 	
 }
